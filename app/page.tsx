9882da27c0af03b635b2cb97d49629d8c3fc956a
nahomjc <aslandjc7@gmail.com>
2024-01-04 10:01:40 -0800

    feat: disney changed to netflex

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,11 @@
+import { Button } from '@/components/ui/button'
 import Image from 'next/image'
 
 export default function Home() {
-  return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-     <div>my disney </div>
+return (
+    <main className="">
+     <h1 className='text-red-500'>Netflix </h1>
+   
     </main>
   )
 }
--- components/Header.tsx
+++ components/Header.tsx
@@ -3,18 +3,21 @@ import Link from 'next/link';
 import React from 'react';
 import { ThemToggler } from './ThemeToggler';
 import SearchInput from './SearchInput';
+import GenreDropdown from './GenreDropdown';
 
 const Header = () => {
   return (
     <header className='fixed w-full z-20 top-0 flex items-center justify-between p-5 bg-gradient-to-t from-gray-200/0 via-gray-900/25 to-gray-900 '>
      <Link href='/'>
-        <Image  src="https://links.papareact.com/a943ae"
-          width={120}
-          height={100}
-          alt="Disney Logo"
-          className={"cursor-pointer inver-0 dark:invert"} />
+        <Image  src="https://cdn4.iconfinder.com/data/icons/logos-and-brands/512/227_Netflix_logo-512.png"
+          width={70}
+          height={30}
+          alt="Netflix logo"
+          className={"cursor-pointer inver-0 "} />
          </Link>
+        
          <div className='flex space-x-2'>
+<GenreDropdown/>
 <SearchInput/>
         <ThemToggler/>
          </div>
