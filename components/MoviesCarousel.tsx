60fe42bec4bdae339efc782f3e05249716e18325
nahomjc <aslandjc7@gmail.com>
2024-01-04 10:10:23 -0800

    feat:movie carsouel added

---
+++ components/MoviesCarousel.tsx
@@ -0,0 +1,18 @@
+import { Movie } from '@/typing';
+import React from 'react'
+
+type Props={
+    title?:string;
+    movies:Movie[];
+    isVertical?:boolean;
+}
+
+function MoviesCarousel() {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default MoviesCarousel
