5b09cd098aa2490ed4415bcccba5505b9fd31b54
nahomjc <aslandjc7@gmail.com>
2024-01-05 16:02:58 -0800

    feat: next config TMDB image added

--- next.config.js
+++ next.config.js
@@ -1,7 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     images:{
-        remotePatterns: [{
+        remotePatterns: [ {
+            protocol: "http",
+            hostname: "image.tmdb.org",
+          },{
             protocol:'https',
             hostname:"links.papareact.com"
         },{
