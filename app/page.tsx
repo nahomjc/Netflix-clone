0524cc14a67fe6d133834b5f22c8948abe2ba204
nahomjc <aslandjc7@gmail.com>
2024-01-05 16:40:03 -0800

    feat: adding style to moviesCarousel

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,24 @@
+import MoviesCarousel from '@/components/MoviesCarousel'
 import { Button } from '@/components/ui/button'
+import { getPopularMovies, getTopRatedMovies, getUpcomingMovies } from '@/lib/getMovies'
 import Image from 'next/image'
 
-export default function Home() {
+export default async function  Home() {
+  const upComingMovies=await getUpcomingMovies() 
+  const topRatedMovies=await getTopRatedMovies()
+  const popularMovies=await getPopularMovies()
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-     <div>my Netflix </div>
+    <main className="">
+     <h1>my Netflix </h1>
+
+     {/* carousel banner wrapper*/}
+
+     <div className='flex flex-col space-y-2 xl:mt-48'>
+      <MoviesCarousel movies={ upComingMovies} title='Upcoming'/>
+      <MoviesCarousel movies={topRatedMovies} title='Top Rated'/>
+      <MoviesCarousel movies={popularMovies} title='Populare'/>
+      {/* MovieCarousel movies{..} title=upcoming*/}
+     </div>
     </main>
   )
 }
--- components/MoviesCarousel.tsx
+++ components/MoviesCarousel.tsx
@@ -13,9 +13,10 @@ function MoviesCarousel({title,movies,isVertical}:Props)  {
   return (
     <div className='z-50'>
       <h2>{title}</h2>
-      {
+      <div className={cn("flex space-x-4 overflow-scroll px-5 lg:px-10 py-5 scrollbar-hide")}>{
         movies?.map(movie=>(<MovieCard key={movie.id} movie={movie}/>))
-      }
+      }</div>
+      
       
     </div>
   )
