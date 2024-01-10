4850fdb80fb2246ed5029ad8a6a4d66238a7f99d
nahomjc <82502560+nahomjc@users.noreply.github.com>
2024-01-10 15:22:38 +0300

    feat: move title style changed

--- components/MoviesCarousel.tsx
+++ components/MoviesCarousel.tsx
@@ -1,25 +1,30 @@
-import { cn } from '@/lib/utils';
-import { Movie } from '@/typing';
-import React from 'react'
-import MovieCard from './MovieCard';
+import { cn } from "@/lib/utils";
+import { Movie } from "@/typing";
+import React from "react";
+import MovieCard from "./MovieCard";
 
-type Props={
-    title?:string;
-    movies:Movie[];
-    isVertical?:boolean;
-}
+type Props = {
+  title?: string;
+  movies: Movie[];
+  isVertical?: boolean;
+};
 
-function MoviesCarousel({title,movies,isVertical}:Props)  {
+function MoviesCarousel({ title, movies, isVertical }: Props) {
   return (
-    <div className='z-50'>
-      <h2>{title}</h2>
-      <div className={cn("flex space-x-4 overflow-scroll px-5 lg:px-10 py-5 scrollbar-hide")}>{
-        movies?.map(movie=>(<MovieCard key={movie.id} movie={movie}/>))
-      }</div>
-      
-      
+    <div className="z-50">
+      <h2 className="text-xl font-bold px-10 py-2">{title}</h2>
+      <div
+        className={cn(
+          "flex space-x-4 overflow-scroll px-5 lg:px-10 py-5 scrollbar-hide",
+          isVertical && "flex-col space-x-0 space-y-12"
+        )}
+      >
+        {movies?.map((movie) => (
+          <MovieCard key={movie.id} movie={movie} />
+        ))}
+      </div>
     </div>
-  )
+  );
 }
 
-export default MoviesCarousel
+export default MoviesCarousel;
