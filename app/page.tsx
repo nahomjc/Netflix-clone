64b20d5520cc12c093f746aa10769041d8ae96d8
nahomjc <82502560+nahomjc@users.noreply.github.com>
2024-01-11 13:04:37 +0300

    feat: CarouselBannerWrapper  added

--- app/page.tsx
+++ app/page.tsx
@@ -1,24 +1,29 @@
-import MoviesCarousel from '@/components/MoviesCarousel'
-import { Button } from '@/components/ui/button'
-import { getPopularMovies, getTopRatedMovies, getUpcomingMovies } from '@/lib/getMovies'
-import Image from 'next/image'
+import CarouselBannerWrapper from "@/components/CarouselBannerWrapper";
+import MoviesCarousel from "@/components/MoviesCarousel";
+import { Button } from "@/components/ui/button";
+import {
+  getPopularMovies,
+  getTopRatedMovies,
+  getUpcomingMovies,
+} from "@/lib/getMovies";
+import Image from "next/image";
 
-export default async function  Home() {
-  const upComingMovies=await getUpcomingMovies() 
-  const topRatedMovies=await getTopRatedMovies()
-  const popularMovies=await getPopularMovies()
+export default async function Home() {
+  const upComingMovies = await getUpcomingMovies();
+  const topRatedMovies = await getTopRatedMovies();
+  const popularMovies = await getPopularMovies();
   return (
     <main className="">
-     <h1>my Netflix </h1>
+      <h1>my Netflix </h1>
 
-     {/* carousel banner wrapper*/}
+      <CarouselBannerWrapper />
 
-     <div className='flex flex-col space-y-2 xl:mt-48'>
-      <MoviesCarousel movies={ upComingMovies} title='Upcoming'/>
-      <MoviesCarousel movies={topRatedMovies} title='Top Rated'/>
-      <MoviesCarousel movies={popularMovies} title='Populare'/>
-      {/* MovieCarousel movies{..} title=upcoming*/}
-     </div>
+      <div className="flex flex-col space-y-2 xl:mt-48">
+        <MoviesCarousel movies={upComingMovies} title="Upcoming" />
+        <MoviesCarousel movies={topRatedMovies} title="Top Rated" />
+        <MoviesCarousel movies={popularMovies} title="Popular" />
+        {/* MovieCarousel movies{..} title=upcoming*/}
+      </div>
     </main>
-  )
+  );
 }
