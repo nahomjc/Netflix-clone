18659d104d9bb665fc36c558689cb2563b6fbc8a
nahomjc <82502560+nahomjc@users.noreply.github.com>
2024-01-11 14:33:35 +0300

    feat: header uncommented

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,37 +1,37 @@
-import type { Metadata } from 'next'
-import { Inter } from 'next/font/google'
-import './globals.css'
-import Header from '@/components/Header'
-import { ThemeProvider } from '@/components/ThemeProvider'
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import "./globals.css";
+import Header from "@/components/Header";
+import { ThemeProvider } from "@/components/ThemeProvider";
+import Footer from "@/components/Footer";
 
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: 'Netflix-clone_AI',
-  description: 'Netflix project for Education',
-}
+  title: "Netflix-clone_AI",
+  description: "Netflix project for Education",
+};
 
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
   return (
-    <html lang="en"  suppressHydrationWarning>
-
-<body className="dark:bg-[#1A1C29] bg-white">
+    <html lang="en" suppressHydrationWarning>
+      <body className="dark:bg-[#1A1C29] bg-white">
         <ThemeProvider
           attribute="class"
           defaultTheme="system"
           enableSystem
           disableTransitionOnChange
         >
-    { /*<Header />*/}
+          <Header />
 
           {children}
-             
+          <Footer />
         </ThemeProvider>
       </body>
     </html>
-  )
+  );
 }
