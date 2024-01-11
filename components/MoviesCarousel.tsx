bffc4655fc04ea15df069e18864b3f0f3239d52d
nahomjc <82502560+nahomjc@users.noreply.github.com>
2024-01-11 13:05:54 +0300

    feat: isVertical condation

--- components/MoviesCarousel.tsx
+++ components/MoviesCarousel.tsx
@@ -10,6 +10,7 @@ type Props = {
 };
 
 function MoviesCarousel({ title, movies, isVertical }: Props) {
+  console.log(movies, "list of movies");
   return (
     <div className="z-50">
       <h2 className="text-xl font-bold px-10 py-2">{title}</h2>
@@ -19,9 +20,27 @@ function MoviesCarousel({ title, movies, isVertical }: Props) {
           isVertical && "flex-col space-x-0 space-y-12"
         )}
       >
-        {movies?.map((movie) => (
-          <MovieCard key={movie.id} movie={movie} />
-        ))}
+        {isVertical
+          ? movies.map((movie) => (
+              <div
+                key={movie.id}
+                className={cn(
+                  isVertical &&
+                    "flex flex-col space-y-5 mb-5 items-center lg:flex-row space-x-5"
+                )}
+              >
+                <MovieCard movie={movie} />
+                <div className="max-w-2xl">
+                  <p className="font-bold">
+                    {movie.title} {movie.release_date?.split("-")[0]}
+                  </p>
+                  <hr className="mb-3" />
+                  <p className="">{movie.overview}</p>
+                </div>
+              </div>
+            ))
+          : movies?.map((movie) => <MovieCard key={movie.id} movie={movie} />)}
+        {}
       </div>
     </div>
   );
