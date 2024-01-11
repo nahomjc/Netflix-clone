224bce7d53ff60d55282eaefd46d92c230ed7d44
nahomjc <82502560+nahomjc@users.noreply.github.com>
2024-01-11 16:19:28 +0300

    chore: updated api

--- lib/getMovies.ts
+++ lib/getMovies.ts
@@ -7,7 +7,7 @@ async function fetchFromTMDB(url: URL, cacheTime?: number) {
   url.searchParams.set("language", "en-US");
   url.searchParams.set("page", "1");
 
-  const option: RequestInit = {
+  const options: RequestInit = {
     method: "GET",
     headers: {
       accept: "application/json",
@@ -17,14 +17,51 @@ async function fetchFromTMDB(url: URL, cacheTime?: number) {
       revalidate: cacheTime || 60 * 60 * 24,
     },
   };
-  const response = await fetch(url.toString(), option);
+
+  const response = await fetch(url.toString(), options);
   const data = (await response.json()) as SearchResults;
   return data;
+}
+
+export async function getDiscoverMovies(id?: string, keywords?: string) {
+  const url = new URL(`https://api.themoviedb.org/3/discover/movie`);
+
+  keywords && url.searchParams.set("with_keywords", keywords);
+  id && url.searchParams.set("with_genres", id);
+
+  const data = await fetchFromTMDB(url);
+  return data.results;
+}
+
+export async function getSearchedMovies(term: string) {
+  const url = new URL("https://api.themoviedb.org/3/search/movie");
+
+  url.searchParams.set("query", term);
+  url.searchParams.set("include_adult", "false");
+  url.searchParams.set("language", "en-US");
+  url.searchParams.set("page", "1");
+
+  const options: RequestInit = {
+    method: "GET",
+    headers: {
+      accept: "application/json",
+      Authorization: `Bearer ${process.env.TMDB_API_KEY}`,
+    },
+    next: {
+      revalidate: 60 * 60 * 24,
+    },
+  };
+
+  const response = await fetch(url.toString(), options);
+  const data = (await response.json()) as SearchResults;
+
+  return data.results;
 }
 
 export async function getUpcomingMovies() {
   const url = new URL("https://api.themoviedb.org/3/movie/upcoming");
   const data = await fetchFromTMDB(url);
+
   return data.results;
 }
 
@@ -39,23 +76,5 @@ export async function getPopularMovies() {
   const url = new URL("https://api.themoviedb.org/3/movie/popular");
   const data = await fetchFromTMDB(url);
 
-  return data.results;
-}
-
-export async function getDiscoverMovies(id?: string, keywords?: string) {
-  const url = new URL(`https://api.themoviedb.org/3/discover/movie`);
-  keywords && url.searchParams.set("with_keywords", keywords);
-  id && url.searchParams.set("with_genres", id);
-
-  const data = await fetchFromTMDB(url);
-  return data.results;
-}
-
-export async function getSearchedMovies(term: string) {
-  const url = new URL(`https://api.themoviedb.org/3/search/movies`);
-
-  url.searchParams.set("query", term);
-
-  const data = await fetchFromTMDB(url);
   return data.results;
 }
