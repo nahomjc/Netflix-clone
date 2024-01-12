e4e22f63c6737453bafd30cb9e0afe2238f3409a
nahomjc <82502560+nahomjc@users.noreply.github.com>
2024-01-12 13:56:29 +0300

    feat:genre view card

--- app/genre/[id]/page.tsx
+++ app/genre/[id]/page.tsx
@@ -1,20 +1,25 @@
-import React from 'react';
+import MoviesCarousel from "@/components/MoviesCarousel";
+import { getDiscoverMovies } from "@/lib/getMovies";
+import React from "react";
 
-type Props={
-    params:{
-        id:string
-    }
-    searchParams:{
-        genre:string
-    }
-}
-const Genrepage = ({params:{id},searchParams:{genre}}:Props) => {
-
+type Props = {
+  params: {
+    id: string;
+  };
+  searchParams: {
+    genre: string;
+  };
+};
+async function GenrePage({ params: { id }, searchParams: { genre } }: Props) {
+  const movies = await getDiscoverMovies(id);
   return (
-    <div>
-      <h1>welcome to gener id{id}: searchParams {genre}</h1>
+    <div className="max-w-7xl mx-auto">
+      <div className="flex flex-col space-y-5 mt-32 xl:mt-42">
+        <h1 className="text-6xl font-bold px-10">Result for {genre}</h1>
+        <MoviesCarousel title={`Genre`} movies={movies} isVertical />
+      </div>
     </div>
   );
 }
 
-export default Genrepage;
+export default GenrePage;
