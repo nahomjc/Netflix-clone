d387031ec838d172784499e4c059a1aa424a4f5c
nahomjc <82502560+nahomjc@users.noreply.github.com>
2024-01-12 13:04:12 +0300

    feat: remove search description

--- components/SearchInput.tsx
+++ components/SearchInput.tsx
@@ -1,10 +1,10 @@
-'use client'
+"use client";
 
-import { useRouter } from 'next/navigation'
-import React from 'react'
-import { useForm } from 'react-hook-form'
-import * as z from 'zod'
-import {zodResolver} from '@hookform/resolvers/zod'
+import { useRouter } from "next/navigation";
+import React from "react";
+import { useForm } from "react-hook-form";
+import * as z from "zod";
+import { zodResolver } from "@hookform/resolvers/zod";
 import {
   Form,
   FormControl,
@@ -13,47 +13,46 @@ import {
   FormItem,
   FormLabel,
   FormMessage,
-} from "@/components/ui/form"
-import { Input } from './ui/input'
+} from "@/components/ui/form";
+import { Input } from "./ui/input";
 const formSchema = z.object({
-    input: z.string().min(2).max(50),
-  })
+  input: z.string().min(2).max(50),
+});
 function SearchInput() {
-    const router =useRouter()
-    const form = useForm<z.infer<typeof formSchema>>({
-        resolver: zodResolver(formSchema),
-        defaultValues: {
-         input: "",
-        },
-      })
-      function onSubmit(values: z.infer<typeof formSchema>) {
-        // Do something with the form values.
-        // ✅ This will be type-safe and validated.
-        console.log(values)
-        router.push(`/search/${values.input}`)
-        form.reset()
-      }
-    
-      return (
-        <Form {...form}>
-          <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
-           <FormField control={form.control} name='input'
-           render={({field})=>(
+  const router = useRouter();
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      input: "",
+    },
+  });
+  function onSubmit(values: z.infer<typeof formSchema>) {
+    // Do something with the form values.
+    // ✅ This will be type-safe and validated.
+    console.log(values);
+    router.push(`/search/${values.input}`);
+    form.reset();
+  }
+
+  return (
+    <Form {...form}>
+      <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
+        <FormField
+          control={form.control}
+          name="input"
+          render={({ field }) => (
             <FormItem>
-             
               <FormControl>
                 <Input placeholder="Search.." {...field} />
               </FormControl>
-              <FormDescription>
-                This is your searching feild.
-              </FormDescription>
+
               <FormMessage />
             </FormItem>
           )}
-           />
-          </form>
-        </Form>
-      )
+        />
+      </form>
+    </Form>
+  );
 }
 
-export default SearchInput
+export default SearchInput;
