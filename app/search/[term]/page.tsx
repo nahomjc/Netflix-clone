5ba98f90773419031a8a0da4c532365743b66da3
nahomjc <82502560+nahomjc@users.noreply.github.com>
2024-01-12 13:27:39 +0300

    feat: added result searched title

--- app/search/[term]/page.tsx
+++ app/search/[term]/page.tsx
@@ -20,6 +20,7 @@ async function SearchPage({ params: { term } }: Props) {
   return (
     <div className="max-w-7xl mx-auto">
       <div className="flex flex-col space-y-4 mt-32 xl:mt-42">
+        <h1 className="text-6xl font-bold px-10">Results for {termToUse}</h1>
         <MoviesCarousel movies={movies} title="Movies" isVertical />
         <MoviesCarousel movies={PopularMovies} title="You may also like" />
       </div>
