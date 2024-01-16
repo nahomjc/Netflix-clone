cee5781a62382e1b376a0b93e05b3bc0b988588c
nahomjc <82502560+nahomjc@users.noreply.github.com>
2024-01-16 17:59:48 +0300

    feat: nav funcation for details

--- components/MovieCard.tsx
+++ components/MovieCard.tsx
@@ -1,21 +1,29 @@
+"use client";
 import getImagePath from "@/lib/getImagePath";
 import { Movie } from "@/typing";
 import Image from "next/image";
-
+import { useRouter } from "next/navigation";
 function MovieCard({ movie }: { movie: Movie }) {
+  const router = useRouter();
+
+  const Navigate = (id: any) => {
+    router.push(`/view/${id}`);
+    console.log(id, "view for detal");
+  };
   return (
-    <div className="flex-shrink-0 relative cursor-pointer transform hover:scale-105 transition duration-200 ease-out hover:drop-shadow-lg">
+    <div className="relative cursor-pointer transform hover:scale-105 transition duration-200 ease-out hover:drop-shadow-lg ">
       <div className="absolute inset-0 bg-gradient-to-b from-gray-200/0 via-gray-900/10 to-gray-300 dark:to-[#1A1C29]/80 z-10" />
 
       <p className="absolute z-20 bottom-5 left-5">{movie.title}</p>
       <Image
         className="w-fit lg:min-w-[400px] h-56 object-cover object-center shadow-md shadow-gray-900 drop-shadow-xl rounded-sm"
+        onClick={() => Navigate(movie.id) as any}
         src={getImagePath(movie.backdrop_path || movie.poster_path)}
         alt={movie.title}
         width={1920}
         height={1080}
         key={movie.id}
-      />{/*movie card details*/}
+      />
     </div>
   );
 }
