f11e9582d63b481521a8609c013e6f792db312dd
nahomjc <82502560+nahomjc@users.noreply.github.com>
2024-01-17 11:56:01 +0300

    feat: sign up and login button

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,28 +1,33 @@
-import Image from 'next/image';
-import Link from 'next/link';
-import React from 'react';
-import { ThemToggler } from './ThemeToggler';
-import SearchInput from './SearchInput';
-import GenreDropdown from './GenreDropdown';
+import Image from "next/image";
+import Link from "next/link";
+import React from "react";
+import { ThemToggler } from "./ThemeToggler";
+import SearchInput from "./SearchInput";
+import GenreDropdown from "./GenreDropdown";
+import { Button } from "./ui/button";
 
 const Header = () => {
   return (
-    <header className='fixed w-full z-20 top-0 flex items-center justify-between p-5 bg-gradient-to-t from-gray-200/0 via-gray-900/25 to-gray-900 '>
-     <Link href='/'>
-        <Image  src="https://cdn4.iconfinder.com/data/icons/logos-and-brands/512/227_Netflix_logo-512.png"
+    <header className="fixed w-full z-20 top-0 flex items-center justify-between p-5 bg-gradient-to-t from-gray-200/0 via-gray-900/25 to-gray-900 ">
+      <Link href="/">
+        <Image
+          src="https://cdn4.iconfinder.com/data/icons/logos-and-brands/512/227_Netflix_logo-512.png"
           width={70}
           height={30}
           alt="Netflix logo"
-          className={"cursor-pointer inver-0 "} />
-         </Link>
-        
-         <div className='flex space-x-2'>
-<GenreDropdown/>
-<SearchInput/>
-        <ThemToggler/>
-         </div>
+          className={"cursor-pointer inver-0 "}
+        />
+      </Link>
+
+      <div className="flex space-x-2">
+        <GenreDropdown />
+        <SearchInput />
+        <ThemToggler />
+        <Button variant="destructive">Sign Up</Button>
+        <Button variant="outline">Login</Button>
+      </div>
     </header>
   );
-}
+};
 
 export default Header;
